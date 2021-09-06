ff3ec62f4427d10e6e06953904c1dec83b4883bc
satoshii <webmaster@satoshii.org>
2021-09-06 17:09:11 +0900

    言語設定をlocalStorageに保存

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { environment } from '../environments/environment';
 import { Title }     from '@angular/platform-browser';
+import { EnvService } from './env.service';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,17 @@ export class AppComponent {
   languages = environment.languages;
   currentLang: string = environment.defaultLang;
 
-  constructor(private title: Title, private translate: TranslateService) {
-    translate.setDefaultLang(environment.defaultLang);
-    translate.use(environment.defaultLang);
-    this.setTitle();
+  constructor(private title: Title, private translate: TranslateService, private envService: EnvService) {
+    envService.getEnv().subscribe(env => {
+      // envServiceから言語設定が取れたらデフォルトを上書きする
+      if(environment.languages.some(language => language.lang === env.lang)) {
+        environment.defaultLang = env.lang!;
+      }
+      translate.setDefaultLang(environment.defaultLang);
+      translate.use(environment.defaultLang);
+      this.currentLang = environment.defaultLang;
+      this.setTitle();
+    });
   }
 
   setTitle() {
@@ -31,7 +39,9 @@ export class AppComponent {
       this.translate.use(lang);
       this.currentLang = lang;
       this.setTitle();
-    }   
+      // 設定を保存
+      this.envService.setEnv({lang:lang}).subscribe();
+    }
   }
 
   isCurrentLang(lang: string): boolean {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   defaultLang: 'en',
-  languages: [ {'lang': 'en', 'label': 'English'}, {'lang': 'ja', 'label': 'Japanese'} ]
+  languages: [ {'lang': 'en', 'label': 'English'}, {'lang': 'ja', 'label': '日本語'} ]
 };
 
 /*
